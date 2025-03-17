24706f83a53f77e0cad3d17fb54729b2582e1535
Sovan Das <sovandas089@gmail.com>
2025-03-17 20:42:10 +0530

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("hello! wellcome to OffSec Journey ")
+
+name = input(
+    "Who are you ?\n"
+)
+if(name =="Psycho"):
+    print("Sorry Man! I can't allow you!!!!")
+
+else:
+    print("hey "+ name +", Most wellcome")
+
