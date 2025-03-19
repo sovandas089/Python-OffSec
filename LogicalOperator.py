b0694562c464110482609a36174697efc368fc29
Sovan Das <sovandas089@gmail.com>
2025-03-20 00:15:42 +0530

    Logical Operator comit

---
+++ LogicalOperator.py
@@ -0,0 +1,19 @@
+print("hello! wellcome to OffSec Journey ")
+
+name = input(
+    "Who are you ?\n"
+)
+#Or is a logical operator
+if(name =="Psycho" or name == "Lawdasur"):
+    psycho_status = input("Are you Psycho?\n")
+    good_deeds = int(input("How many good deeds have you done\n"))
+    #and is a logical operator
+    #It is a nested if
+    if psycho_status == "Yes" and good_deeds < 5:
+     print("Sorry Man! I can't allow you!!!!")
+     #Here is Nested else
+    else:
+       print("Most wellcome Brother") 
+else:
+    print("hey "+ name +", Most wellcome")
+ 
