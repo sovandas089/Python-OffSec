aec119317cfebbee2a5c5941cf2e08883579fb39
Sovan Das <sovandas089@gmail.com>
2025-03-24 20:54:57 +0530

    List Commit

---
+++ List.py
@@ -0,0 +1,21 @@
+# This is List in Python
+
+camping_stuff = "tent, sleeping bags, water, coffee, knife, ethernet cable, router, flash drive, laptop, headset"
+
+print(camping_stuff)
+
+camping_list = ["tent", "sleeping bags", "water", "coffee", "knife", "ethernet cable", "router", "flash drive", "laptop", "headset"
+]
+
+print(type(camping_list))
+print(camping_list)
+
+camp_site = ["Lake", 304, 20.0, False]
+
+me = camping_list[3]
+you = camping_list[-1]
+# -1 prints the last indexed Value
+
+
+print(me)
+print(you)
